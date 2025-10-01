83134dce0c8a78c5107dbf56fb50b958dbcc9f09
Martons00 <raffaelemartone34@gmail.com>
2025-10-01 15:36:19 +0200

    Update SwinUNETREncoder.py

--- models/SwinUNETREncoder.py
+++ models/SwinUNETREncoder.py
@@ -46,6 +46,8 @@ if __name__ == "__main__":
     print(model)
     new_model = SwinUNETREncoder(model)  # Modello con solo l'encoder
     print(new_model)
+    new_model.num_features = 768  # Aggiungiamo l'attributo num_features
     from ML_Decoder_main.src_files.ml_decoder.ml_decoder import add_ml_decoder_head
+
     new_model = add_ml_decoder_head(new_model, num_classes=3)
     print(new_model)
