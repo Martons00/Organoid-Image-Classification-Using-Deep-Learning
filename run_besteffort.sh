63e77e6523a31cf38fead179a766a9363406cb40
Martons00 <mraffael@fsophia>
2025-10-21 11:37:50 +0200

    Esperimenti Noah

--- run_besteffort.sh
+++ run_besteffort.sh
@@ -14,5 +14,5 @@ pwd
 source models/SwinUNETR/BRATS21/swin_unetr_env/bin/activate
 
 # Avvia il training
-python train.py --cfg config/SwinUNETR/training_lr_5e-4_cs_ce_pt.yaml --oar_id $OAR_JOB_ID
+python train.py --cfg config/SwinUNETR/training_lr_1e-4_big_p.yaml --oar_id $OAR_JOB_ID
 exit
--- run_noah.sh
+++ run_noah.sh
@@ -13,5 +13,5 @@ pwd
 source models/SwinUNETR/BRATS21/swin_unetr_env/bin/activate
 
 # Avvia il training
-python train.py --cfg config/SwinUNETR+NOAH/training_lr_5e-4.yaml --oar_id $OAR_JOB_ID
+python train.py --cfg config/SwinUNETR+NOAH/training_lr_1e-3_pt.yaml --oar_id $OAR_JOB_ID
 exit
