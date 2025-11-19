152268e2d0a2444c3ce556c7f674f8208391ce86
Martons00 <mraffael@fsophia>
2025-11-19 10:41:31 +0100

    Tables sorted

--- tables/sort.sh
+++ tables/sort.sh
@@ -1,5 +1,7 @@
 #python sort_md_table.py results_validation/light/all_result_reduced.md --key ValAcc --desc --output results_validation/light/Sorted_all_result_reduced.md
 #python sort_md_table.py results_validation/light/all_result_full.md --key ValAcc --desc --output results_validation/light/Sorted_all_result_full.md
-cd results && python filter_markdown_table.py && cd ..
-python sort_md_table.py results/light/00_reduced_results.md --key TestAcc --desc --output results/Sorted_all_results_reduced.md
-python sort_md_table.py results/light/00_full_results.md --key TestAcc --desc --output results/Sorted_all_results_full.md
+#cd results && python filter_markdown_table.py && cd ..
+#python sort_md_table.py results/light/00_reduced_results.md --key TestAcc --desc --output results/Sorted_all_results_reduced.md
+#python sort_md_table.py results/light/00_full_results.md --key TestAcc --desc --output results/Sorted_all_results_full.md
+python sort_md_table.py results/full/00_full_results.md --key TestAcc --desc --output results/full/01_Sorted_full_results.md
+python sort_md_table.py results/full/00_reduced_results.md --key TestAcc --desc --output results/full/01_Sorted_reduced_results.md
